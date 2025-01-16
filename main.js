17ce0d9f3916e837810f45dd34987e2d3cc2f74d
Shanti Rai <shantirai@MacBookAir.lan>
2025-01-16 18:03:36 +0000

    small ammendments

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 This is a series of string challemges to help you get to grips with JS string methods! For other in the series go here: ......
 
 Hello again challenger! This time your task is to write a function named `removeVowels` which takes in a string and returns the string with all vowels removed. 
-If input is an empty string, it should return the message "There is nothing to remove!". If it is missing an input, then it should THROW an error message "There is no input to check!"
-
+If input is an empty string, it should return the message "There is nothing to remove!". 
+If it is missing an input, then it should THROW an error message "There is no input to check!"
+It doesn't matter if the letters are lower or uppercase, ALL vowels need to be removed.
 
 
 Example(Input --> Output)
@@ -14,7 +15,7 @@ removeVowels() --> throws error "Input string is required!"
 "I enjoy solving coding challenges daily." --> " njy slvng cdng chllngs dly."
 "UsE PlAywRiGHt fOr Api TesTinG!" -->  "s PlywRGHt fr p TsTnG!"
 
-Note: it doesn't matter if the letters are lower or uppercase, ALL vowels need to be removed.
+Note: 
 If unsure about throw Error check https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
 */
 
